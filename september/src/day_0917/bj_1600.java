ad46889b3871e0c0081b391decf5a6f6456cf4a9
ilhwan <ikhwan62@naver.com>
2021-09-18 12:18:05 +0900

    0916 문제풀이

--- september/src/day_0917/bj_1600.java
+++ september/src/day_0917/bj_1600.java
@@ -98,3 +98,122 @@ public class bj_1600 {
 		System.out.println(-1);
 	}
 }
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//import java.util.StringTokenizer;
+//
+//public class test {
+//
+//	static int k, n, m;
+//	static int[][] map;
+//	static boolean[][][] v;
+//	static int[] dx = { 1, 0, -1, 0 };
+//	static int[] dy = { 0, 1, 0, -1 };
+//	static int[] hdx = { -2, -2, -1, -1, 1, 1, 2, 2 };
+//	static int[] hdy = { 1, -1, 2, -2, 2, -2, 1, -1 };
+//
+//	static class loc {
+//		int x;
+//		int y;
+//		int dis;
+//		int jump;
+//
+//		public loc(int x, int y, int dis, int jump) {
+//			this.x = x;
+//			this.y = y;
+//			this.dis = dis;
+//			this.jump = jump;
+//		}
+//
+//	}
+//
+//	static class xy {
+//		int x;
+//		int y;
+//		int jump;
+//
+//		public xy(int x, int y, int jump) {
+//			this.x = x;
+//			this.y = y;
+//			this.jump = jump;
+//		}
+//
+//	}
+//
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//		k = Integer.parseInt(st.nextToken());
+//
+//		st = new StringTokenizer(br.readLine(), " ");
+//		m = Integer.parseInt(st.nextToken());
+//		n = Integer.parseInt(st.nextToken());
+//
+//		map = new int[n][m];
+//		v = new boolean[n][m][k + 1];
+//		for (int i = 0; i < n; i++) {
+//			st = new StringTokenizer(br.readLine(), " ");
+//			for (int j = 0; j < m; j++) {
+//				map[i][j] = Integer.parseInt(st.nextToken());
+//			}
+//		}
+//
+//		int ans = mzs(0, 0);
+//
+//		System.out.println(ans);
+//
+//	}
+//
+//	static int mzs(int x, int y) {
+//		Queue<loc> q = new LinkedList<>();
+//		q.add(new loc(x, y, 0, 0));
+//		v[0][0][0] = true;
+//		while (!q.isEmpty()) {
+//			loc cur = q.poll();
+//			if (cur.x == n - 1 && cur.y == m - 1)
+//				return cur.dis;
+//			for (int i = 0; i < 4; i++) {
+//				int nx = cur.x + dx[i];
+//				int ny = cur.y + dy[i];
+//				if (range(nx, ny)) {
+//					if (map[nx][ny] == 0) {
+//						if (!v[nx][ny][cur.jump]) {
+//							q.offer(new loc(nx, ny, cur.dis + 1, cur.jump));
+//							v[nx][ny][cur.jump] = true;
+//						}
+//					}
+//				}
+//			}
+//
+//			if (cur.jump == k)
+//				continue;
+//			
+//			for (int i = 0; i < 8; i++) {
+//				int nx = cur.x + hdx[i];
+//				int ny = cur.y + hdy[i];
+//				if (range(nx, ny)) {
+//					if (map[nx][ny] == 0) {
+//						if (!v[nx][ny][cur.jump + 1]) {
+//							if (cur.jump < k) {
+//								q.offer(new loc(nx, ny, cur.dis + 1, cur.jump + 1));
+//								v[nx][ny][cur.jump + 1] = true;
+//							}
+//						}
+//					}
+//				}
+//			}
+//
+//		}
+//
+//		return -1;
+//	}
+//
+//	static boolean range(int x, int y) {
+//		return x >= 0 && x < n && y >= 0 && y < m;
+//	}
+//}
